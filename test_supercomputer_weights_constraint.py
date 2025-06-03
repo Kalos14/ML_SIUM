c5cffd7dc5844899fd897a85b87f66f3aa5902c6
Gabriele Calandrino <gabriele.calandrino@epfl.ch>
2025-06-04 01:03:22 +0200

    change in epochs and lr

--- test_supercomputer_weights_constraint.py
+++ test_supercomputer_weights_constraint.py
@@ -131,13 +131,13 @@ X_non_num = stock_data[columns_to_drop_in_x]
 
 
 window = 60
-epoch = 10
+epoch = 20
 K = 10
 D = stock_data.shape[1] - len(columns_to_drop_in_x)
 H = 1
 dF = 256
 ridge_penalty = 10
-lr = 1e-4
+lr = 1e-5
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 run_artifacts = {

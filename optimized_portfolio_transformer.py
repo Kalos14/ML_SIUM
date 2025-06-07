1bef54417ff088a6b27090d3ea1c97df233d1cfe
federicosabbatanischiuma <federico.sabbatanischiuma@epfl.ch>
2025-06-07 13:40:22 +0200

    .

--- optimized_portfolio_transformer.py
+++ optimized_portfolio_transformer.py
@@ -21,6 +21,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset
+import os
 
 ################################################################################
 # 1.  Helper utilities

8f6dc40ec946247052012ac2070f59921b3668ad
federicosabbatanischiuma <federico.sabbatanischiuma@epfl.ch>
2025-06-09 22:47:25 +0200

    Large on clusterr with penalties

--- sr_vs_complexity.py
+++ sr_vs_complexity.py
@@ -16,7 +16,7 @@ stock_data = pd.read_pickle(dataset_path)
 # stock_data = stock_data[stock_data["size_grp"] == "large"]
 
 # stock_data = pd.read_pickle('our_version_norm.pkl')
-size_group = 'small'
+size_group = 'large'
 if size_group is not None:
   stock_data = stock_data.loc[stock_data.size_grp==size_group]
 
@@ -152,7 +152,7 @@ for P in complexities:
 # Save Sharpe ratios to CSV
 sharpe_df = pd.DataFrame.from_dict(sharpe_by_P, orient='index')  # rows = P, cols = λ
 sharpe_df.index.name = "P"
-sharpe_df.to_csv(os.path.join(output_dir, "sharpe_ratios_by_P.csv"))
+sharpe_df.to_csv(os.path.join(output_dir, "sharpe_ratios_by_P_large.csv"))
 print("Saved Sharpe ratios to CSV.")
 
 # Save cumulative returns to CSV
@@ -161,7 +161,7 @@ returns_df = pd.DataFrame({
     for (P, shrink), ret in returns_by_P.items()
 })
 returns_df.index.name = "Date"
-returns_df.to_csv(os.path.join(output_dir, "cumulative_returns_by_P.csv"))
+returns_df.to_csv(os.path.join(output_dir, "cumulative_returns_by_P_large.csv"))
 print("Saved cumulative returns to CSV.")
 
 # Plot cumulative returns
